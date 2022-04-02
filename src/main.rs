c5aa61424425615db8abf07a8212f8999c0b8e24
ricardo <ricardo_griffith@msn.com>
2022-04-02 19:31:00 -0300

    Added logic to skip executable from args vector.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@ use std::fs::File;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let iter_args = &args;
+    let mut do_not_skip_it = false;
 
-            for item in iter_args {
-                println!("{:?}", item);
-     // Create file in write-only mode, returns `io::Result<File>`
-                match File::create(item) {
-                    Err(why) => panic!("couldn't create {}: {}", item, why),
-                    Ok(file) => file,
-                };
-            }
+    for item in iter_args {
+        //println!("{:?}", item);
 
+        // Ignore the first item
+        if do_not_skip_it
+        {
+            // Create file in write-only mode, returns `io::Result<File>`
+            match File::create(item) {
+                Err(why) => panic!("couldn't create {}: {}", item, why),
+                Ok(file) => file,
+            };
+        }
+
+        // Ensures processing flags, not the executable.
+        do_not_skip_it = true;
     }
+}
